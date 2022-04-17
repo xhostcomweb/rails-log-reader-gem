7a32d8b9b6cc3946005d2491f3f57918dcd3042b
Paul Anthony McGowan <xhostcom@gmail.com>
2022-04-17 05:47:28 +0100

    Gitacora saved 10 changed files

--- lib/log_reader.rb
+++ lib/log_reader.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 require_relative "log_reader/version"
+require_relative "log_reader/file_reader"
+require_relative "log_reader/entry"
+require_relative "log_reader/tokenizer"
+require_relative "log_reader/accumulator"
+require_relative: "log_reader/aggregator"
 
 module LogReader
   class Error < StandardError; end

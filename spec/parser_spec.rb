379f653798622e95188694c9ca61b0cf57a571ba
Paul Anthony McGowan <xhostcom@gmail.com>
2022-04-17 14:30:34 +0100

    Gitacora saved 18 changed files

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,4 +1,11 @@
-RSpec.describe LogParser::Parser do
+RSpec.describe LogReader::Parser do
   let(:parser) { described_class.new("some/file") }
 
+  describe "#format" do
+    subject { parser.format } 
+
+    it "returns a hash" do 
+
+    end
+  end
 end

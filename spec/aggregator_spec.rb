d9ff29aa485dff50a4e24c62d0ae254f87f54b75
Paul Anthony McGowan <xhostcom@gmail.com>
2022-04-17 07:09:04 +0100

    Gitacora saved 10 changed files

--- spec/aggregator_spec.rb
+++ spec/aggregator_spec.rb
@@ -2,11 +2,8 @@ RSpec.describe LogReader::Aggregator do
   let(:aggregator) { described_class.new(entries) }
   let(:entries) {
        [
-        LogReader::Entry.new("home", "8.3.4.3"),
          LogReader::Entry.new("about", "4.3.4.3"),
-         LogReader::Entry.new("home", "8.3.4.3"),
-         LogReader::Entry.new("about", "4.3.4.3"),
-        LogReader::Entry.new("home", "8.3.4.3")
+         LogReader::Entry.new("home", "8.3.4.3")
        ] 
     } 
 describe "#group" do 

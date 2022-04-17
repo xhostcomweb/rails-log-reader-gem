615215fff5d154b3984db2613e4b62cbe64b40f8
Paul Anthony McGowan <xhostcom@gmail.com>
2022-04-17 11:35:00 +0100

    Gitacora saved 17 changed files

--- spec/formatter_spec.rb
+++ spec/formatter_spec.rb
@@ -1,3 +1,10 @@
 RSpec.describe LogReader::Formatter do
-  let(:formatter) { described_class.new }
+  let(:formatter) { described_class.new(aggregator_result) } 
+  let(:aggregator_result) { 
+    LogReader::AggregatorResult[
+      "/about" => instance_double(LogParser::Accumulator, unique: 5, total: 100),
+      "/home" => instance_double(LogParser::Accumulator, unique: 10, total: 25),
+      "/contact" => instance_double(LogParser::Accumulator, unique: 2, total: 140)
+    ]
+   }
 end

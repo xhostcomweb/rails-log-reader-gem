fdb45fce842bb867c54694c061447e03d7c2c467
Paul Anthony McGowan <xhostcom@gmail.com>
2022-04-17 05:51:16 +0100

    Gitacora saved 9 changed files

---
+++ lib/log_reader/aggregator.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module LogReader
+   class Aggregator < AbstractAggregator
+      attr_accessor :total, :unique, :ips
+
+      def initialize(entries) 
+      end
+
+      def group 
+
+      end
+   end
+end

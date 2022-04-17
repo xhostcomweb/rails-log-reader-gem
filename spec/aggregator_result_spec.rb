b2ed05a4b08ad794af2e60eb671de22a3e39f678
Paul Anthony McGowan <xhostcom@gmail.com>
2022-04-17 09:47:43 +0100

    Gitacora saved 10 changed files

--- spec/aggregator_result_spec.rb
+++ spec/aggregator_result_spec.rb
@@ -1,4 +1,10 @@
 RSpec.describe LogReader::AggregatorResult do 
-
+  let(:result) { described_class[grouped_entries] } 
+  let(:grouped_entries) {
+      { |variable| variable.path }.each_with_object(AggregatorResult.new) do |(path, entries), collection|
+        collection[path] ||= Accumulator.new
+        collection[path].add(entries)
+      end
+  }
 
 end

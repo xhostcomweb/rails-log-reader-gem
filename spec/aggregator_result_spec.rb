12a3587184a19e0b929ef8b1ef3c8c2431c9824c
Paul Anthony McGowan <xhostcom@gmail.com>
2022-04-17 09:53:05 +0100

    Gitacora saved 10 changed files

--- spec/aggregator_result_spec.rb
+++ spec/aggregator_result_spec.rb
@@ -1,10 +1,13 @@
 RSpec.describe LogReader::AggregatorResult do 
   let(:result) { described_class[grouped_entries] } 
   let(:grouped_entries) {
-      { |variable| variable.path }.each_with_object(AggregatorResult.new) do |(path, entries), collection|
-        collection[path] ||= Accumulator.new
-        collection[path].add(entries)
-    end 
-  }
-
+      { 
+          "/about" => accumulator1,
+          "/home" => accumulator2,
+          "/contact" => accumulator3
+      }
+ }
+let(:accumulator1) { instance_double(LogReader::Accumulator, unique: 5, total: 5) } 
+let(:accumulator2) { instance_double(LogReader::Accumulator, unique: 10, total: 25) } 
+let(:accumulator3) { instance_double(LogReader::Accumulator, unique: 2, total: 150) }
 end

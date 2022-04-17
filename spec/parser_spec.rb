7e01137c1856aa8ae12ed480f5cd26d47de6874a
Paul Anthony McGowan <xhostcom@gmail.com>
2022-04-17 15:03:31 +0100

    Gitacora saved 17 changed files

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,20 +1,16 @@
 RSpec.describe LogReader::Parser do
-  let(:parser) { described_class.new("somefile") }
+ let(:parser) { described_class.new("some/file") }
 
   describe "#format" do
-    subject { parser.format } 
+    subject { parser.format }
 
     let(:reader) {
-      instance_double(LogReader::FileReader, lines: ["/about 1,1,1,1", "/contact 2.2.2.2"])
-    } 
+      instance_double(LogReader::FileReader, lines: ["/about-us 1.1.1.1", "/contact 2.2.2.2"])
+    }
 
-    before do 
-      allow(LogReader::FileReader).to receive("somefile").and_return(reader)
+    before do
+      allow(LogReader::FileReader).to receive(:new).with("some/file").and_return(reader)
     end
-
-    #it "returns a hash" do 
-     # expect(subject).to be_a(Hash)
-   # end 
 
     it { is_expected.to include("Entries by total views") }
   end

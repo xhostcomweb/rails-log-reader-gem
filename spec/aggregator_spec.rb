b03f05aa255cd133c2e674fc07d7d1ffd0dbe0a6
Paul Anthony McGowan <xhostcom@gmail.com>
2022-04-17 07:14:51 +0100

    Gitacora saved 10 changed files

--- spec/aggregator_spec.rb
+++ spec/aggregator_spec.rb
@@ -11,14 +11,14 @@ RSpec.describe LogReader::Aggregator do
     } 
 describe "#group" do 
     subject(:group) { aggregator.group }
-   # it "should return a hash" do 
-      #  expect(group).to be_a(Hash)
-    #end
+    it "should return a hash" do 
+        expect(group).to be_a(Hash)
+    end
     #it "should return a hash with 2 keys" do 
        # expect(group.keys.size).to eq(2)
     # end 
-    it "should group entries by their path" do
-      expect(group.keys).to match_array(["/about", "home"])
-    end
+    #it "should group entries by their path" do
+     # expect(group.keys).to match_array(["/about", "home"])
+   # end
   end
 end

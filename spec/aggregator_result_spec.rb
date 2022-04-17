88d5dc2b327cd37ad03d8ddeb7743fec58a05e5d
Paul Anthony McGowan <xhostcom@gmail.com>
2022-04-17 10:29:22 +0100

    Gitacora saved 10 changed files

--- spec/aggregator_result_spec.rb
+++ spec/aggregator_result_spec.rb
@@ -17,6 +17,9 @@ describe "~sort_by_total_views" do
   let(:expected_sorted_entries) { 
         [
             
+          "/about" => accumulator1,
+          "/home" => accumulator2,
+          "/contact" => accumulator3
         ]
     }
 end

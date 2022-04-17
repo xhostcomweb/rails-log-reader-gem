a164b305215ec1262cd4db5509bab8980ea33bc2
Paul Anthony McGowan <xhostcom@gmail.com>
2022-04-17 12:31:41 +0100

    Gitacora saved 16 changed files

--- spec/formatter_spec.rb
+++ spec/formatter_spec.rb
@@ -1,30 +1,32 @@
+# frozen_string_literal: true
+
 RSpec.describe LogReader::Formatter do
-  let(:formatter) { described_class.new(aggregator_result) } 
-  let(:aggregator_result) { 
+  let(:formatter) { described_class.new(aggregator_result) }
+  let(:aggregator_result) do
     LogReader::AggregatorResult[
       "/about" => instance_double(LogReader::Accumulator, unique: 5, total: 100),
       "/home" => instance_double(LogReader::Accumulator, unique: 10, total: 25),
       "/contact" => instance_double(LogReader::Accumulator, unique: 2, total: 140)
     ]
-   } 
-   
-   describe "#format" do 
-   subject { formatter.format} 
+  end
 
-   let(:expected_output) {
-<<-EOF 
+  describe "#format" do
+    subject { formatter.format }
+
+    let(:expected_output) do
+      <<-EOF
         Entries by total views:
-        /contact 140 visits
-        /about 100 visits
-        /home 25 visits
+        /contact 140 views
+        /about 100 views
+        /home 25 views
 
         Entries by unique views:
-        /home 10 unique visits
-        /about 5 unique visits
-        /contact 2 unique visits
-    EOF
-   } 
+        /home 10 unique views
+        /about 5 unique views
+        /contact 2 unique views
+      EOF
+    end
 
-   it { is_expected.to eql(expected_output) }
-end 
+    it { is_expected.to eql(expected_output) }
+  end
 end

038387e294d01c2a78481a61212cf1c3b358ebaf
Paul Anthony McGowan <xhostcom@gmail.com>
2022-04-17 06:25:19 +0100

    Gitacora saved 10 changed files

--- spec/aggregator_spec.rb
+++ spec/aggregator_spec.rb
@@ -18,6 +18,7 @@ describe "#group" do
        # expect(group.keys.size).to eq(2)
     # end 
     it "should group entries by path" do 
-        
+        expect(group.keys).to match_array(["/home", "/about"])
+        end
 
  end

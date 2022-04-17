fe0ee565bf747d96e2406cdf814c6b0da0264897
Paul Anthony McGowan <xhostcom@gmail.com>
2022-04-17 09:07:36 +0100

    Gitacora saved 10 changed files

--- lib/log_reader/aggregator_result.rb
+++ lib/log_reader/aggregator_result.rb
@@ -1,11 +1,5 @@
-module LogReader
+module LogReader 
     class AggregatorResult < Hash
-    def sort_by_total
-     # sort_by { |_, views| -views.total }
+    
     end
-
-    def sort_by_unique
-     # sort_by { |_, views| -views.unique }
-    end
-  end
 end

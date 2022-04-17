8ee6752ab0d5103261be21adf789667790167ebe
Paul Anthony McGowan <xhostcom@gmail.com>
2022-04-17 11:45:28 +0100

    Gitacora saved 16 changed files

---
+++ lib/log_reader/formatter.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module LogReader
+  class Formatter
+    def initialize(aggregator_result)
+      @aggregator_result = aggregator_result
+    end
+
+    def format
+
+    end
+  end
+end

f5ae6e9168b8bbc301b0c36e55836fa95eebc18f
Paul Anthony McGowan <xhostcom@gmail.com>
2022-04-17 08:54:15 +0100

    Gitacora saved 10 changed files

--- lib/log_reader.rb
+++ lib/log_reader.rb
@@ -5,7 +5,8 @@ require_relative "log_reader/file_reader"
 require_relative "log_reader/entry"
 require_relative "log_reader/tokenizer"
 require_relative "log_reader/accumulator"
-require_relative "log_reader/aggregator"
+require_relative "log_reader/aggregator" 
+require_relative "log_reader/aggregator_result"
 
 module LogReader
   class Error < StandardError; end

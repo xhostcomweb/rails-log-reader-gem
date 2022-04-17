3ca45dc8f7fe485d8f459c1d3da290c088c7d886
Paul Anthony McGowan <xhostcom@gmail.com>
2022-04-17 05:55:28 +0100

    Gitacora saved 9 changed files

---
+++ spec/aggregator_spec.rb
@@ -0,0 +1,12 @@
+RSpec.describe LogReader::Aggregator do 
+  let(:aggregator) { described_class.new(entries) }
+  let(:entries) {
+       [
+        LogReader::Entry.new("home", "8.3.4.3"),
+         LogReader::Entry.new("home", "8.3.4.3"),
+         LogReader::Entry.new("home", "8.3.4.3"),
+         LogReader::Entry.new("home", "8.3.4.3"),
+        LogReader::Entry.new("home", "8.3.4.3")
+       ] 
+    }
+  let(:entry1) { LogReader::Entry.new("home", "

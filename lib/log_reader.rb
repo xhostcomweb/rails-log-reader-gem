eb5946f0f26138ac204fd686298775a716cb7f25
Paul Anthony McGowan <xhostcom@gmail.com>
2022-04-19 06:38:50 +0100

    Comments

--- lib/log_reader.rb
+++ lib/log_reader.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 require_relative "log_reader/version"
-require_relative "log_reader/file_reader"
-require_relative "log_reader/entry"
-require_relative "log_reader/tokenizer"
-require_relative "log_reader/accumulator"
-require_relative "log_reader/aggregator"
-require_relative "log_reader/aggregator_result"
-require_relative "log_reader/formatter"
-require_relative "log_reader/parser"
+require_relative "log_reader/file_reader" # Reads the file in entire lines (by path and ip)
+require_relative "log_reader/entry"       # Use Struct to group the lines by attributes in to hash object of quoted strings, path & IP, to view entry, path, ip, totals, uniques
+require_relative "log_reader/tokenizer"   # Map the entries in to an array of line entries
+require_relative "log_reader/accumulator" # Count each entry by path related ip, incrementing view count/unique view count by ip
+require_relative "log_reader/aggregator"  # Group entries by path, store no of entries for each path
+require_relative "log_reader/aggregator_result" # Sort the entries by total & unique views by testing against a predetermined descending or ascending order
+require_relative "log_reader/formatter" # Format these split in to entries by total and unique views for output
+require_relative "log_reader/parser" # Place the entire above sequence in to single parser file for output to screen, refactor for reuse/extend for different input types
 
 module LogReader
   class Error < StandardError
